3ab1d1fa4e01cd5be9573dd9647dafb30329fba0
Dave86dev <davidochando1986@gmail.com>
2022-12-22 13:18:44 +0100

    hot fix

--- config/auth.js
+++ config/auth.js
@@ -2,7 +2,7 @@
 
 module.exports = {
 
-    SECRET: process.env.SECRET || "tic tac toe ho ho ho", 
-    EXPIRES: process.env.EXPIRES || "24h",
-    ROUNDS : process.env.ROUNDS || 10
+    SECRET: process.env.AUTH_SECRET || "tic tac toe ho ho ho", 
+    EXPIRES: process.env.AUTH_EXPIRES || "24h",
+    ROUNDS : process.env.AUTH_ROUNDS || 10
 };
